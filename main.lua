c643f5730f2e3cc8a87f07941563417678ee7d92
Elizabeth Brayton Benard <ebrayton@gmail.com>
2017-10-04 09:41:50 -0600

    Mixing image and text drawing

--- main.lua
+++ main.lua
@@ -1,6 +1,11 @@
 function love.load()
-forgive = love.graphics.newImage("ff.jpg")
+    forgive = love.graphics.newImage("ff.jpg")
+    love.graphics.setNewFont(15)
+    -- love.graphics.setColor(0,0,0)  -- causes image to be drawn as a black square
+    love.graphics.setBackgroundColor(255,255,255)
 end
+
 function love.draw()
-love.graphics.draw(forgive, 100, 100)
+    love.graphics.draw(forgive, 0, 0)
+    love.graphics.print("Hello World!", 200, 450)
 end
